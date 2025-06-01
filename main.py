f75573bc9a76903f7f2fa0c1bd6d56926552548f
Caspar Siemssen <c.siemssen@campus.tu-berlin.de>
2025-06-01 23:30:11 +0200

    fix main intern eval call

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from src.eval.eval_models import evaluate_model
+from src.models.intern_vl_inference import InternVLInferenceEngine
 
 # Todo
 evaluate_model(
-    engine="intern_vl",
+    engine=InternVLInferenceEngine("OpenGVLab/InternVL3-2B"),
     dataset_split="val",
     batch_size=2,
 )
