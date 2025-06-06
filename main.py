da3062f953bbc11c21d9dcef02da358f8075f311
Caspar Siemssen <c.siemssen@campus.tu-berlin.de>
2025-06-06 12:56:26 +0200

    update main to setup device specific quantization

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from src.eval.eval_models import evaluate_model
 from src.models.intern_vl_inference import InternVLInferenceEngine
+form src.utils.utils import is_cuda
 
-if __name__ == '__main__':
-  # todo
-  evaluate_model(
-      engine=InternVLInferenceEngine("OpenGVLab/InternVL3-2B"),
-      dataset_split="val",
-      batch_size=2,
-  )
+if __name__ == "__main__":
+    # Todo
+    if is_cuda():
+        engine = InternVLInferenceEngine(
+            "OpenGVLab/InternVL3-2B",
+            use_4bit=True,
+            torch_dtype=torch.bfloat16,
+        )
+    else:
+        engine = InternVLInferenceEngine("OpenGVLab/InternVL3-2B")
+
+    evaluate_model(
+        engine=engine,
+        dataset_split="val",
+        batch_size=2,
+    )
