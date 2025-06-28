b5c197fa86253641335576660a21e95fc7d54567
Veit Laule <83905032+vDawgg@users.noreply.github.com>
2025-06-28 17:14:06 +0200

    Fix OOM issues in finetuning loop (#68)
    * add config from qlora paper, remove superfluous code, disable flash_attn for training
    
    * disable wandb reporting
    
    * Lower cuda version for compatibility with server
    
    * add batch size as cli arg, ensure same base image in docker, cleanup
    
    * remove descriptor qas for eval as they are only used for training

--- src/train/train_qwen.py
+++ src/train/train_qwen.py
@@ -20,12 +20,12 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from typing import Any, Optional
 
-import pandas as pd
 import transformers
-from peft import LoraConfig
-from qwen_vl_utils import process_vision_info
+import pandas as pd
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
 from transformers import Trainer
 from transformers.trainer import ALL_LAYERNORM_LAYERS, get_parameter_names
+from qwen_vl_utils import process_vision_info
 
 from src.constants import model_log_dir, model_output_dir
 from src.data.basic_dataset import DriveLMImageDataset
@@ -63,19 +63,6 @@ def log_trainable_parameters(model):
     logger.info(
         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
     )
-
-
-def set_model(model):
-    """
-    Set grad to True for LLM part of the model only.
-    """
-    for n, p in model.visual.named_parameters():
-        p.requires_grad = False
-    for n, p in model.visual.merger.named_parameters():
-        p.requires_grad = False
-    for n, p in model.model.named_parameters():
-        p.requires_grad = True
-    model.lm_head.requires_grad = True
 
 
 def create_optimizer(self):
@@ -258,7 +245,8 @@ def create_optimizer(self):
 # TODO: Look into the deepspeed config
 def train(
     approach_name: str,
-    test_set_size: Optional[int | None] = None,
+    batch_size: str,
+    test_set_size: Optional[str] = None,
     use_grid: bool = False,
     use_augmented: bool = False,
 ):
@@ -299,20 +287,8 @@ def train(
         dataset = create_subset_for_testing(dataset, int(test_set_size))
     dataset = [message for message, _, _, _, _ in dataset]
 
-    engine.load_model()
-
-    if hasattr(engine.model, "enable_input_require_grads"):
-        engine.model.enable_input_require_grads()
-    else:
-
-        def make_inputs_require_grad(module, input, output):
-            output.requires_grad_(True)
-
-        engine.model.get_input_embeddings().register_forward_hook(
-            make_inputs_require_grad
-        )
-
-    set_model(engine.model)
+    engine.load_model(flash_attn=False)
+    model = prepare_model_for_kbit_training(engine.model, use_gradient_checkpointing=True)
 
     lora_config = LoraConfig(
         lora_alpha=16,
@@ -322,22 +298,21 @@ def train(
         target_modules=["q_proj", "v_proj"],
         task_type="CAUSAL_LM",
     )
-    engine.model.add_adapter(lora_config)
-    engine.model.config.use_cache = False
-    engine.model.gradient_checkpointing_enable()
-
-    log_trainable_parameters(engine.model)
+    model = get_peft_model(engine.model, lora_config)
+    log_trainable_parameters(model)
 
     trainer = Trainer(
-        model=engine.model,
+        model=model, 
         processing_class=engine.processor.tokenizer,
         args=TrainingArguments(
+            report_to="none",
             remove_unused_columns=False,
             bf16=True,
-            tf32=True,
             output_dir=model_output_dir / name,
             num_train_epochs=1,
-            auto_find_batch_size=True,
+            per_device_train_batch_size=int(batch_size),
+            gradient_accumulation_steps=4,
+            warmup_steps=2,
         ),
         train_dataset=dataset,
         data_collator=collator,
