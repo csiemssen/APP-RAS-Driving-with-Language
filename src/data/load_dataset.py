1ab6d3df7064ddf00f6be70dd6ea4d90156b123d
csiemssen <100309871+csiemssen@users.noreply.github.com>
2025-07-01 13:42:37 +0200

    fix missing sample dir in val zip extraction path (#73)

--- src/data/load_dataset.py
+++ src/data/load_dataset.py
@@ -38,7 +38,7 @@ def get_ds(split: str) -> None:
             id="18f8ygNxGZWat-crUjroYuQbd39Sk9xCo",
             output=out_name,
         )
-        extract_children(out_name, nuscenes_dir)
+        extract_children(out_name, nuscenes_dir / "samples")
         gdown.download(
             id="1fsVP7jOpvChcpoXVdypaZ4HREX1gA7As",
             output=os.path.join(drivelm_dir, "v1_1_val_nus_q_only.json"),
