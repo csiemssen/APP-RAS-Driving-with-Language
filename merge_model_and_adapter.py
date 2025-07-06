231ca5adbaf40c07bca2128a8b0b9cb6af47b231
Veit Laule <83905032+vDawgg@users.noreply.github.com>
2025-07-06 21:47:49 +0200

    make sure to merge adapter with basemodel and add model and trainer outputs to git and dockerignore. (#87)

--- merge_model_and_adapter.py
+++ merge_model_and_adapter.py
@@ -2,6 +2,7 @@ import os
 from argparse import ArgumentParser
 
 from peft import PeftModel
+from transformers import Qwen2_5_VLForConditionalGeneration
 
 from src.constants import model_dir, model_output_dir
 from src.models.qwen_vl_inference import QwenVLInferenceEngine
@@ -15,12 +16,11 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-engine = QwenVLInferenceEngine(use_4bit=True)
-engine.load_model()
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-3B-Instruct")
 
 model = PeftModel.from_pretrained(
-    model=engine.model,
+    model=model,
     model_id=os.path.join(model_output_dir, args.adapter),
 )
-model = model.merge_and_unload()
-model.save_pretrained(os.path.join(model_dir, args.adapter))
+test = model.merge_and_unload()
+test.save_pretrained(os.path.join(model_dir, args.adapter))
