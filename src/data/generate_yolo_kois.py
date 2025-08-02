18d8b9aefb5e94f5029659d0ace18986084d16ab
Veit Laule <veit.laule@gmail.com>
2025-08-02 14:14:47 +0200

    restrict yolo classes, mute yolo logging

--- src/data/generate_yolo_kois.py
+++ src/data/generate_yolo_kois.py
@@ -1,5 +1,6 @@
 import os
 
+from tqdm import tqdm
 from ultralytics import YOLO
 
 from src.constants import drivelm_dir
@@ -7,13 +8,18 @@ from src.constants import drivelm_dir
 
 def generate_yolo_kois(data, max_results_per_cam: int = 5):
     model = YOLO("yolo11n.pt")
-    for _, scene_obj in data.items():
+    for _, scene_obj in tqdm(data.items(), desc="Generating KOIs with YOLO"):
         for _, key_frame in scene_obj["key_frames"].items():
             image_paths_raw = key_frame["image_paths"]
             i = 0
             kois = []
             for camera, image_path in image_paths_raw.items():
-                results = model(os.path.join(drivelm_dir, image_path))[:max_results_per_cam]
+                results = model(
+                    os.path.join(drivelm_dir, image_path), 
+                    max_det=max_results_per_cam,
+                    classes=[0, 1, 2, 3, 5, 6, 7, 9, 11], # [person, bicycle, car, motorcycle, bus, train, truck, traffic light, stop sign]
+                    verbose=False
+                )
                 bbox = [xyxy for res in results for xyxy in res.boxes.xyxy.cpu().tolist()]
                 center_points = [
                     (xywh[0], xywh[1]) for res in results for xywh in res.boxes.xywh.cpu()
